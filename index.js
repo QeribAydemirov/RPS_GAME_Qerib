e43641cd2c99b702f21c4e653dedb6fc1598eb25
Cahangir Asgerov <109364812+cahangirasgerov2003@users.noreply.github.com>
2026-03-25 20:35:24 +0400

    fix: index.js error

--- index.js
+++ index.js
@@ -1,75 +1,61 @@
 let scoreMe = document.querySelektor(".scoreMe"),
-scoreCOM = document.querySelector(".scoreCOM"),
-myLastScore = document.querySelector(".myLastScoreMe"),
-comLastScre = document.querySelector(".comLastScore"),
-myChoiseImage = document.querySelector(".myChoiseImg > img"),
-comChoiseImage = document.querySelector(".comChiseImg > img");
+  scoreCOM = document.querySelector(".scoreCOM"),
+  myLastScore = document.querySelector(".myLastScoreMe"),
+  comLastScre = document.querySelector(".comLastScore"),
+  myChoiseImage = document.querySelector(".myChoiseImg > img"),
+  comChoiseImage = document.querySelector(".comChiseImg > img");
 let canPress = true;
 let maxScore = 0;
+let myChoise = null;
 
 let choises = [
+  { img: "/assets/image/1.jpg", title: "Rock" },
 
-{ img: "/assets/image/1.jpg",
-  title: "Rock",
-},
+  {
+    img: "/assets/image/2.jpg",
+    title: "Paper",
+  },
 
-{
- img: "/assets/image/2.jpg",
- title: "Paper",
-},
-
-{
- img: "/assets/image/3.jpg",
- title: "Scissors",
-},
+  {
+    img: "/assets/image/3.jpg",
+    title: "Scissors",
+  },
 ];
 
 class keybordGame {
-    comChooseyourChoise() {
+  comChooseyourChoise() {}
 
+  userChooseYourChoise(key) {
+    key = key.toUpperCase();
+
+    this.comChooseyourChoise();
+
+    if (!["r", "p", "s"].includes(key)) {
+      alert("R P S - den birini click ede bilersen !!!");
     }
-}
-userChooseYourChoise(key) {
-     key = key.toUpperCase();
 
-     this.comChooseYourChoise();
-}
+    if (key === "r") {
+      myChoise = 0;
+    } else if (key === "p") {
+      myChoise = 1;
+    } else {
+      myChoise = 2;
+    }
 
+    this.showChoises();
+  }
 
-if (!["r","p","s"].includes(key)) {
-alert("R P S - den birini click ede bilersen !!!")
+  showChoises() {
+    myChoiseImage.src = choises[myChoise].img;
+    myChoiseTitle.innerHTML = choises[myChoise].title;
 
-
-
-console.log("Sehv duyme clicklenmesi");
-
-return;
-}
-
-if (key === "r") {
-    myChoise = 0;
-} else if (key === "p") {
-    myChoise = 1;
-} else {
-    myChoise = 2;
+    comChoiseImage.src = choises[comChoise].img;
+    myChoiseTitle.innerHTML = choises[comChoise].title;
+  }
 }
 
- this.showChoises();
-
-
-showChoises() {
-  myChoiseImage.src = choises[myChoise].img;
-  myChoiseTitle.innerHTML = choises[myChoise].title;
-
-  comChoiseImage.src = choises[comChoise].img;
-  myChoiseTitle.innerHTML = choises[comChoise].title;
-
-}
- 
 let rpsGame = new keybordGame();
 
 window.addEventListener("keydown", (e) => {
-rpsGame.userChooseYourChoise(e.key);
+  rpsGame.userChooseYourChoise(e.key);
 });
-
-
